3c9ea229a04a314a4850b0c0c12c02071b671569
C1291420 Max Rabelo do Nascimento <c1291420@interno.bb.com.br>
2023-05-26 13:24:18 -0300

    instalando native base

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { View, StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <View /> : <View />}
+    </NativeBaseProvider>
+  );
+}
